820bb6ed315336a8f148a5894a2aa59732827252
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2023-09-12 15:43:29 -0600

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import CharacterSheet from "$lib/CharacterSheet/CharacterSheet.svelte";
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+  <CharacterSheet />
+</div>
